2fceb75d2c72ccf04e7f293e8e8222f2d0ebfba6
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-11-22 21:25:09 -0500

    removed build from gitignore

---
+++ client/build/precache-manifest.b318d28ead35121aa3645a7ceb3453dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8a8bf021cad74cb0f369f345d79ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58886f53dd5bbb6084e5",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "bd92470391b54f14eccb",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "58886f53dd5bbb6084e5",
+    "url": "/static/js/2.7a6822be.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.7a6822be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd92470391b54f14eccb",
+    "url": "/static/js/main.fb4367ad.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
